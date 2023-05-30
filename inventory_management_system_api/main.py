b61d9d29cf47cc5ae7b778dc59278cc13f024a25
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-05-30 16:31:20 +0000

    Configure linter

--- inventory_management_system_api/main.py
+++ inventory_management_system_api/main.py
@@ -1,4 +1,6 @@
-
+"""
+Main module contains the API entrypoint.
+"""
 from fastapi import FastAPI
 
 API_DESCRIPTION = "This is the API for the Inventory Management System"
@@ -7,4 +9,7 @@ app = FastAPI(title="Inventory Management System API", description=API_DESCRIPTI
 
 @app.get("/")
 def read_root():
+    """
+    Root endpoint for the API.
+    """
     return {"Title": "Inventory Management System API"}
