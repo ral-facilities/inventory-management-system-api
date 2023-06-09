5768ba147fac0df5100c3941b746c7c9109b5710
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-09 17:02:11 +0100

    Implement an endpoint for creating a catalogue category #2

--- inventory_management_system_api/main.py
+++ inventory_management_system_api/main.py
@@ -7,12 +7,15 @@ from fastapi import FastAPI
 
 from inventory_management_system_api.core.config import config
 from inventory_management_system_api.core.logger_setup import setup_logger
+from inventory_management_system_api.routers.v1 import catalogue_category
 
 app = FastAPI(title=config.api.title, description=config.api.description)
 
 setup_logger()
 logger = logging.getLogger()
 logger.info("Logging now setup")
+
+app.include_router(catalogue_category.router)
 
 
 @app.get("/")
