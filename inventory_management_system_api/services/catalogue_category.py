512d402def3e9133d356a1eba76128987576c266
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-09 16:54:21 +0100

    Implement a service method for creating a catalogue category #2

--- inventory_management_system_api/services/catalogue_category.py
+++ inventory_management_system_api/services/catalogue_category.py
@@ -3,7 +3,9 @@ Module for providing a service for managing catalogue categories using the Categ
 """
 from fastapi import Depends
 
+from inventory_management_system_api.models.catalogue_category import CatalogueCategoryIn, CatalogueCategoryOut
 from inventory_management_system_api.repositories.catalogue_category import CatalogueCategoryRepo
+from inventory_management_system_api.schemas.catalogue_category import CatalogueCategoryPostRequestSchema
 
 
 class CatalogueCategoryService:
@@ -18,3 +20,14 @@ class CatalogueCategoryService:
         :param catalogue_category_repository: The catalogue category repository to use.
         """
         self.catalogue_category_repository = catalogue_category_repository
+
+    def create(self, catalogue_category: CatalogueCategoryPostRequestSchema) -> CatalogueCategoryOut:
+        """
+        Create a new catalogue category.
+
+        :param catalogue_category: The catalogue category to be created.
+        :return: The created catalogue category.
+        """
+        return self.catalogue_category_repository.create(
+            CatalogueCategoryIn(**catalogue_category.dict())
+        )
