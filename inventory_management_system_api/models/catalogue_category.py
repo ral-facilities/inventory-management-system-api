c24a49c096228b4a9d48a8ae3b2e103ec6b0ec1c
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-09 16:46:10 +0100

    Allow catalogue category parent IDs to be passed as strings to db models #2

--- inventory_management_system_api/models/catalogue_category.py
+++ inventory_management_system_api/models/catalogue_category.py
@@ -6,13 +6,39 @@ from typing import Optional
 from bson import ObjectId
 from pydantic import BaseModel, Field
 
+from inventory_management_system_api.core.exceptions import InvalidObjectIdError
+
+
+class ObjectIdField(ObjectId):
+    """
+    Custom field for handling MongoDB ObjectId validation.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str) -> ObjectId:
+        """
+        Validate if the string value is a valid ObjectId.
+
+        :param value: The string value to be validated.
+        :return: The validated ObjectId.
+        :raises InvalidObjectIdError: If the value is an invalid ObjectId.
+        """
+        if not ObjectId.is_valid(value):
+            field_name = cls.__name__
+            raise InvalidObjectIdError(f"Invalid ObjectId value for '{field_name}' field")
+        return ObjectId(value)
+
 
 class CatalogueCategoryIn(BaseModel):
     """
     Input database model for a catalogue category.
     """
     name: str
-    parent_id: Optional[ObjectId] = None
+    parent_id: Optional[ObjectIdField] = None
 
 
 class CatalogueCategoryOut(CatalogueCategoryIn):
