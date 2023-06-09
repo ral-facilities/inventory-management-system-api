4f7a36dfede0c5bcc9238bcde1d0900595f7f395
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-09 16:33:43 +0100

    Set up the logging system #2

--- inventory_management_system_api/main.py
+++ inventory_management_system_api/main.py
@@ -1,11 +1,18 @@
 """
 Main module contains the API entrypoint.
 """
+import logging
+
 from fastapi import FastAPI
 
 from inventory_management_system_api.core.config import config
+from inventory_management_system_api.core.logger_setup import setup_logger
 
 app = FastAPI(title=config.api.title, description=config.api.description)
+
+setup_logger()
+logger = logging.getLogger()
+logger.info("Logging now setup")
 
 
 @app.get("/")
