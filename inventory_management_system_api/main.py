62add955a725efe898faf7eaca6fb567bb9af808
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-06-12 09:56:03 +0100

    Fix CORS issues #2

--- inventory_management_system_api/main.py
+++ inventory_management_system_api/main.py
@@ -4,6 +4,7 @@ Main module contains the API entrypoint.
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from inventory_management_system_api.core.config import config
 from inventory_management_system_api.core.logger_setup import setup_logger
@@ -14,6 +15,17 @@ app = FastAPI(title=config.api.title, description=config.api.description)
 setup_logger()
 logger = logging.getLogger()
 logger.info("Logging now setup")
+
+# Fixes CORS issues but should be updated before deploying to prod
+ALLOWED_ORIGINS = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(catalogue_category.router)
 
