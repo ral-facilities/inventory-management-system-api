59f8d8ddbe5eb8a523e895e36a11409430261472
Viktor Bozhinov <45173816+VKTB@users.noreply.github.com>
2023-07-24 15:02:45 +0100

    Do not allow unit to be supplied for boolean catalogue item property #6

--- inventory_management_system_api/schemas/catalogue_category.py
+++ inventory_management_system_api/schemas/catalogue_category.py
@@ -2,9 +2,9 @@
 Module for defining the API schema models for representing catalogue categories.
 """
 from enum import Enum
-from typing import Optional, List
+from typing import Optional, List, Any, Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class CatalogueItemPropertyType(str, Enum):
@@ -26,6 +26,24 @@ class CatalogueItemProperty(BaseModel):
     type: CatalogueItemPropertyType
     unit: Optional[str] = None
     mandatory: bool
+
+    @validator("unit")
+    @classmethod
+    def validate_unit(cls, unit_value: str, values: Dict[str, Any]) -> Optional[str]:
+        """
+        Validator for the `unit` field.
+
+        It checks if the `type` of the catalogue item property is a `boolean` and if a` unit` has been specified. It
+        raises a `ValueError` if this is the case.
+
+        :param unit_value: The value of the `unit` field.
+        :param values: The values of the model fields.
+        :return: The value of the `unit` field.
+        :raises ValueError: If `unit` is provided when `type` is set to `boolean`.
+        """
+        if "type" in values and values["type"] == CatalogueItemPropertyType.BOOLEAN and unit_value is not None:
+            raise ValueError(f"Unit not allowed for boolean catalogue item property '{values['name']}'")
+        return unit_value
 
 
 class CatalogueCategoryPostRequestSchema(BaseModel):
