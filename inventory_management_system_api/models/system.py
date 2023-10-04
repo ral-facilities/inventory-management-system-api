c57e6a01ebe84fec6d4c9414be7b95989230667b
Joel Davies <joel.davies@stfc.ac.uk>
2023-10-04 10:21:09 +0100

    Add initial database models for System

---
+++ inventory_management_system_api/models/system.py
@@ -0,0 +1,30 @@
+"""
+Module for defining the database models for representing a System
+"""
+
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from inventory_management_system_api.models.custom_object_id_data_types import CustomObjectIdField, StringObjectIdField
+
+
+class SystemIn(BaseModel):
+    """
+    Input database model for a System
+    """
+
+    name: str
+    location: str
+    owner: str
+    importance: str
+    parent_id: Optional[CustomObjectIdField] = None
+
+
+class SystemOut(SystemIn):
+    """
+    Output database model for a System
+    """
+
+    id: StringObjectIdField = Field(alias="_id")
+    parent_id: Optional[StringObjectIdField] = None
