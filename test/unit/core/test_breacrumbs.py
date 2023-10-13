4093a69c3bb9754df8f9c3b40e86693327890871
Joel Davies <joel.davies@stfc.ac.uk>
2023-10-13 12:51:40 +0100

    Fix breadcrumbs unit tests when run together #87

--- test/unit/core/test_breacrumbs.py
+++ test/unit/core/test_breacrumbs.py
@@ -27,16 +27,17 @@ MOCK_ENTITIES = [
 
 
 @pytest.mark.parametrize(
-    "mock_service,expected_entity_type",
-    [(Mock(CatalogueCategoryService), "catalogue category"), (Mock(SystemService), "system")],
+    "mock_service_type,expected_entity_type",
+    [(CatalogueCategoryService, "catalogue category"), (SystemService, "system")],
 )
 class TestComputeBreadcrumbsWhenValid:
     """Test compute_breadcrumbs functions correctly when it has no errors"""
 
-    def _test_compute_breadcrumbs(self, mock_service, mock_entities, expected_trail_length, expected_full_trail):
+    def _test_compute_breadcrumbs(self, mock_service_type, mock_entities, expected_trail_length, expected_full_trail):
         """Utility function that tests compute_breadcrumbs and asserts a successful response
         is as expected"""
 
+        mock_service = Mock(mock_service_type)
         mock_service.get.side_effect = mock_entities
         result = breadcrumbs.compute_breadcrumbs(mock_entities[0].id, mock_service)
 
@@ -53,47 +54,48 @@ class TestComputeBreadcrumbsWhenValid:
         assert result.full_trail is expected_full_trail
 
     # pylint:disable=W0613
-    def test_compute_breadcrumbs_with_single_breadcrumb(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_with_single_breadcrumb(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs functions correctly when there are no parent entities to look up
         """
         self._test_compute_breadcrumbs(
-            mock_service=mock_service,
+            mock_service_type=mock_service_type,
             mock_entities=[MOCK_ENTITIES[-1]],
             expected_trail_length=1,
             expected_full_trail=True,
         )
 
-    def test_compute_breadcrumbs_with_maximum_trail_length(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_with_maximum_trail_length(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs functions correctly when the breadcrumb trail length required is
         equal to the maximum
         """
         self._test_compute_breadcrumbs(
-            mock_service=mock_service,
+            mock_service_type=mock_service_type,
             # Want last BREADCRUMBS_TRAIL_MAX_LENGTH so last in trail has no parent
             mock_entities=MOCK_ENTITIES[-BREADCRUMBS_TRAIL_MAX_LENGTH:],
             expected_trail_length=BREADCRUMBS_TRAIL_MAX_LENGTH,
             expected_full_trail=True,
         )
 
-    def test_compute_breadcrumbs_when_exceeding_maximum_trail_length(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_when_exceeding_maximum_trail_length(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs functions correctly when the breadcrumb trail length is longer than
         the maximum i.e. the full trail is not returned
         """
         self._test_compute_breadcrumbs(
-            mock_service=mock_service,
+            mock_service_type=mock_service_type,
             mock_entities=MOCK_ENTITIES,
             expected_trail_length=BREADCRUMBS_TRAIL_MAX_LENGTH,
             expected_full_trail=False,
         )
 
-    def test_compute_breadcrumbs_when_first_entity_id_invalid(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_when_first_entity_id_invalid(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs raises an InvalidObjectIdError when the first entity looked
         up has an invalid id
         """
+        mock_service = Mock(mock_service_type)
         mock_error = InvalidObjectIdError("Some error message")
         mock_service.get.side_effect = mock_error
 
@@ -104,11 +106,12 @@ class TestComputeBreadcrumbsWhenValid:
 
         assert str(exc.value) == str(mock_error)
 
-    def test_compute_breadcrumbs_when_first_entity_not_found(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_when_first_entity_not_found(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs raises an EntityNotFoundError when the first entity looked
         up doesn't exist
         """
+        mock_service = Mock(mock_service_type)
         mock_service.get.side_effect = [None]
 
         with pytest.raises(EntityNotFoundError) as exc:
@@ -118,11 +121,12 @@ class TestComputeBreadcrumbsWhenValid:
 
         assert str(exc.value) == f"{expected_entity_type.capitalize()} with ID entity_id was not found"
 
-    def test_compute_breadcrumbs_when_second_entity_id_invalid(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_when_second_entity_id_invalid(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs raises a DatabaseIntegrityError when the second entity looked
         up has an invalid id
         """
+        mock_service = Mock(mock_service_type)
         mock_error = InvalidObjectIdError("Some error message")
         mock_service.get.side_effect = [MOCK_ENTITIES[0], mock_error]
 
@@ -136,11 +140,12 @@ class TestComputeBreadcrumbsWhenValid:
             "breadcrumbs for entity_id"
         )
 
-    def test_compute_breadcrumbs_when_second_entity_not_found(self, mock_service, expected_entity_type):
+    def test_compute_breadcrumbs_when_second_entity_not_found(self, mock_service_type, expected_entity_type):
         """
         Test compute_breadcrumbs raises a DatabaseIntegrityError when the second entity looked
         up doesn't exist
         """
+        mock_service = Mock(mock_service_type)
         mock_service.get.side_effect = [MOCK_ENTITIES[0], None]
 
         with pytest.raises(DatabaseIntegrityError) as exc:
