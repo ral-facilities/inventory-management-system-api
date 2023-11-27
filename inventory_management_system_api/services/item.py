eaaeedf9bf795d0232c3c53562afab43178f66fb
VKTB <45173816+VKTB@users.noreply.github.com>
2023-11-27 16:43:34 +0000

    Implement a service method for creating an item #11

--- inventory_management_system_api/services/item.py
+++ inventory_management_system_api/services/item.py
@@ -1,12 +1,23 @@
 """
-Module for providing a service for managing items using the `ItemRepo` repository.
+Module for providing a service for managing items using the `ItemRepo`, `CatalogueCategoryRepo`, and `CatalogueItemRepo`
+repositories.
 """
 
 import logging
 
 from fastapi import Depends
 
+from inventory_management_system_api.core.exceptions import (
+    MissingRecordError,
+    DatabaseIntegrityError,
+    InvalidObjectIdError,
+)
+from inventory_management_system_api.models.item import ItemOut, ItemIn
+from inventory_management_system_api.repositories.catalogue_category import CatalogueCategoryRepo
+from inventory_management_system_api.repositories.catalogue_item import CatalogueItemRepo
 from inventory_management_system_api.repositories.item import ItemRepo
+from inventory_management_system_api.schemas.item import ItemPostRequestSchema
+from inventory_management_system_api.services import utils
 
 logger = logging.getLogger()
 
@@ -16,9 +27,55 @@ class ItemService:
     Service for managing items.
     """
 
-    def __init__(self, item_repository: ItemRepo = Depends(ItemRepo)) -> None:
+    def __init__(
+        self,
+        item_repository: ItemRepo = Depends(ItemRepo),
+        catalogue_category_repository: CatalogueCategoryRepo = Depends(CatalogueCategoryRepo),
+        catalogue_item_repository: CatalogueItemRepo = Depends(CatalogueItemRepo),
+    ) -> None:
         """
-        Initialise the `ItemService` with an `ItemRepo` repo.
+        Initialise the `ItemService` with an `ItemRepo`, `CatalogueCategoryRepo`, and `CatalogueItemRepo` repos.
 
-        :param item_repository: The `ItemRepo` repository to use."""
+        :param item_repository: The `ItemRepo` repository to use.
+        :param catalogue_category_repository: The `CatalogueCategoryRepo` repository to use.
+        :param catalogue_item_repository: The `CatalogueItemRepo` repository to use.
+        """
         self._item_repository = item_repository
+        self._catalogue_category_repository = catalogue_category_repository
+        self._catalogue_item_repository = catalogue_item_repository
+
+    def create(self, item: ItemPostRequestSchema) -> ItemOut:
+        """
+        Create a new item.
+
+        :param item: The item to be created.
+        :return: The created item.
+        :raises MissingRecordError: If the catalogue item does not exist.
+        """
+        catalogue_item_id = item.catalogue_item_id
+        catalogue_item = self._catalogue_item_repository.get(catalogue_item_id)
+        if not catalogue_item:
+            raise MissingRecordError(f"No catalogue item found with ID: {catalogue_item_id}")
+
+        try:
+            catalogue_category_id = catalogue_item.catalogue_category_id
+            catalogue_category = self._catalogue_category_repository.get(catalogue_category_id)
+            if not catalogue_category:
+                raise DatabaseIntegrityError(f"No catalogue category found with ID: {catalogue_category_id}")
+        except InvalidObjectIdError as exc:
+            raise DatabaseIntegrityError(str(exc)) from exc
+
+        defined_properties = catalogue_category.catalogue_item_properties
+        supplied_properties = item.catalogue_item_override_properties if item.catalogue_item_override_properties else []
+        supplied_properties = utils.process_catalogue_item_properties(
+            defined_properties, supplied_properties, skip_missing_mandatory_check=True
+        )
+
+        return self._item_repository.create(
+            ItemIn(
+                **{
+                    **item.model_dump(),
+                    "catalogue_item_override_properties": supplied_properties,
+                }
+            )
+        )
