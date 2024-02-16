3892062d42409b0efcf2b146b7e532bad2ee309a
Joel Davies <joel.davies@stfc.ac.uk>
2024-02-16 14:09:40 +0000

    Apply to catalogue categories, catalogue items, items & manufacturers #100

--- inventory_management_system_api/models/mixins.py
+++ inventory_management_system_api/models/mixins.py
@@ -6,36 +6,36 @@ and functionality
 from datetime import datetime, timezone
 from typing import Optional
 
-from pydantic import AwareDatetime, BaseModel, Field, model_validator
+from pydantic import AwareDatetime, BaseModel, model_validator
 
 
 class CreatedModifedAtInMixin(BaseModel):
     """
     Input model mixin that provides creation and modfied time fields
 
-    For a create request an instance of the model should be created without supplying the `created_at` field
+    For a create request an instance of the model should be created without supplying the `created_time` field
     as this will cause it to be assigned as the current time.
-    When updating, the `created_at` time should be given so that it is kept the same. The `updated_at` will be
+    When updating, the `created_time` time should be given so that it is kept the same. The `modified_time` will be
     assigned regardless as it is assumed that a new instance will be created only when creating/updating a
     database entry.
     """
 
-    created_at: AwareDatetime = datetime.now(timezone.utc)
-    updated_at: Optional[AwareDatetime] = None
+    created_time: AwareDatetime = datetime.now(timezone.utc)
+    modified_time: Optional[AwareDatetime] = None
 
     @model_validator(mode="after")
     def validator(self) -> "CreatedModifedAtInMixin":
         """
-        Validator that assigns the created_at and updated_at times.
+        Validator that assigns the created_time and modified_time times.
 
-        When `updated_at` is None, which occurrs when not assigning data from an existing database model this
-        assigns the `updated_at` time to be the same as the `created_at` to ensure they are identical. When
-        `updated_at` is defined then it is reassigned as its assumed it already exists and is now being updated.
+        When `modified_time` is None, which occurrs when not assigning data from an existing database model this
+        assigns the `modified_time` time to be the same as the `created_time` to ensure they are identical. When
+        `modified_time` is defined then it is reassigned as its assumed it already exists and is now being updated.
         """
-        if self.updated_at is None:
-            self.updated_at = self.created_at
+        if self.modified_time is None:
+            self.modified_time = self.created_time
         else:
-            self.updated_at = datetime.now(timezone.utc)
+            self.modified_time = datetime.now(timezone.utc)
         return self
 
 
@@ -44,5 +44,5 @@ class CreatedModifiedAtOutMixin(BaseModel):
     Output model mixin that provides creation and modfied time fields
     """
 
-    created_at: AwareDatetime
-    updated_at: AwareDatetime
+    created_time: AwareDatetime
+    modified_time: AwareDatetime
--- inventory_management_system_api/schemas/mixins.py
+++ inventory_management_system_api/schemas/mixins.py
@@ -10,5 +10,5 @@ class CreatedModifiedSchemaMixin(BaseModel):
     Output schema mixin that provides creation and modfied time fields
     """
 
-    created_at: AwareDatetime = Field(description="The date and time this entity was created")
-    updated_at: AwareDatetime = Field(description="The date and time this entity was last updated")
+    created_time: AwareDatetime = Field(description="The date and time this entity was created")
+    modified_time: AwareDatetime = Field(description="The date and time this entity was last updated")
