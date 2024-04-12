d0d3664ea7f86bbe6ae68c3daf08b2f88eedd75c
Joel Davies <joel.davies@stfc.ac.uk>
2024-04-12 08:26:53 +0000

    Move startup to start-mongodb.sh script #229

---
+++ .github/scripts/start-mongodb.sh
@@ -0,0 +1,22 @@
+# Generate keyfile for replica sets and set file permissions
+echo "Generating keyfile"
+openssl rand -base64 756 > ./mongodb/keys/rs_keyfile
+sudo chmod 0400 ./mongodb/keys/rs_keyfile
+sudo chown 999:999 ./mongodb/keys/rs_keyfile
+
+# Setup mongodb and wait 10 seconds for it to initialise
+echo "Starting MongoDB"
+docker compose pull mongo-db
+docker compose up -d --wait --wait-timeout 30 mongo-db
+sleep 10
+
+echo "Intialising MongoDB replica set"
+docker exec -i mongodb_container mongosh --username 'root' --password 'example' --authenticationDatabase=admin --eval "rs.initiate({ _id : 'rs0', members: [{ _id: 0, host: 'localhost:27017' }]})"
+
+# Fix permission issue when setting up python afterwards, it seems to try and access the data directory but doesn't
+# have permission to once edited
+sudo chmod -R 777 ./mongodb/data
+
+echo "::group::Checking replica set status"
+docker exec -i mongodb_container mongosh --username 'root' --password 'example' --authenticationDatabase=admin --eval "rs.status()"
+echo "::endgroup::"
