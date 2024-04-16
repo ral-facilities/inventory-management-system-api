1f961ab95193c72acd218c0b3dd893a817fcc278
Joshua Kitenge <joshua.kitenge@stfc.ac.uk>
2024-04-16 14:13:55 +0000

    remove duplicate code by inheriting a class

--- inventory_management_system_api/models/catalogue_item_property_template.py
+++ inventory_management_system_api/models/catalogue_item_property_template.py
@@ -2,26 +2,17 @@
 Module for defining the database models for representing a Catalogue item property template
 """
 
-from typing import Optional
+from pydantic import ConfigDict, Field
 
-from pydantic import BaseModel, ConfigDict, Field
-
-from inventory_management_system_api.models.catalogue_category import AllowedValues
+from inventory_management_system_api.models.catalogue_category import CatalogueItemProperty
 from inventory_management_system_api.models.custom_object_id_data_types import StringObjectIdField
 
-# pylint: disable=duplicate-code
 
-
-class CatalogueItemPropertyTemplateOut(BaseModel):
+class CatalogueItemPropertyTemplateOut(CatalogueItemProperty):
     """
     Model representing a catalogue item property template.
     """
 
     id: StringObjectIdField = Field(alias="_id")
-    name: str
-    type: str
-    unit: Optional[str] = None
-    mandatory: bool
-    allowed_values: Optional[AllowedValues] = None
 
     model_config = ConfigDict(populate_by_name=True)
--- inventory_management_system_api/schemas/catalogue_item_property_template.py
+++ inventory_management_system_api/schemas/catalogue_item_property_template.py
@@ -2,27 +2,14 @@
 Module for defining the API schema models for representing catalogue item property templates.
 """
 
-from typing import Optional
+from pydantic import Field
 
-from pydantic import BaseModel, Field
-
-
-from inventory_management_system_api.schemas.catalogue_category import AllowedValuesSchema, CatalogueItemPropertyType
+from inventory_management_system_api.schemas.catalogue_category import CatalogueItemPropertySchema
 
 
-# pylint: disable=duplicate-code
-class CatalogueItemPropertyTemplateSchema(BaseModel):
+class CatalogueItemPropertyTemplateSchema(CatalogueItemPropertySchema):
     """
     Schema model representing a catalogue item property template.
     """
 
     id: str = Field(description="ID of the catalogue item property template")
-    name: str = Field(description="The name of the property")
-    type: CatalogueItemPropertyType = Field(description="The type of the property")
-    unit: Optional[str] = Field(default=None, description="The unit of the property such as 'nm', 'mm', 'cm' etc")
-    mandatory: bool = Field(description="Whether the property must be supplied when a catalogue item is created")
-    allowed_values: Optional[AllowedValuesSchema] = Field(
-        default=None,
-        description="Definition of the allowed values this property can take. 'null' indicates any value matching the "
-        "type is allowed.",
-    )
