fb79682c566f4955d71cc88f5b5eed87fc1838a1
Joel Davies <joel.davies@stfc.ac.uk>
2024-04-29 11:47:36 +0000

    Add check that catalogue category is a leaf node and add some queries #259

--- inventory_management_system_api/services/catalogue_category_property.py
+++ inventory_management_system_api/services/catalogue_category_property.py
@@ -6,8 +6,12 @@ propagation down through their child catalogue items and items using their respe
 import logging
 from fastapi import Depends
 
-from inventory_management_system_api.core.exceptions import InvalidActionError
-from inventory_management_system_api.models.catalogue_category import CatalogueItemPropertyIn, CatalogueItemPropertyOut
+from inventory_management_system_api.core.exceptions import InvalidActionError, MissingRecordError
+from inventory_management_system_api.models.catalogue_category import (
+    CatalogueCategoryIn,
+    CatalogueItemPropertyIn,
+    CatalogueItemPropertyOut,
+)
 from inventory_management_system_api.repositories.catalogue_category import CatalogueCategoryRepo
 from inventory_management_system_api.repositories.catalogue_item import CatalogueItemRepo
 from inventory_management_system_api.repositories.item import ItemRepo
@@ -64,18 +68,64 @@ class CatalogueCategoryPropertyService:
             raise InvalidActionError("Cannot add a mandatory property without a default value")
 
         # Obtain the existing catalogue category to validate against
-        catalogue_category = self._catalogue_category_repository.get(catalogue_category_id)
+        stored_catalogue_category = self._catalogue_category_repository.get(catalogue_category_id)
+        if not stored_catalogue_category:
+            raise MissingRecordError(f"No catalogue category found with ID: {catalogue_category_id}")
+
+        # Must be a leaf catalogue category in order to have properties
+        if not stored_catalogue_category.is_leaf:
+            raise InvalidActionError("Cannot add a property to a non-leaf catalogue category")
 
         # Ensure the property is actually valid
         utils.check_duplicate_catalogue_item_property_names(
-            catalogue_category.catalogue_item_properties + [catalogue_item_property]
+            stored_catalogue_category.catalogue_item_properties + [catalogue_item_property]
         )
 
         catalogue_item_property_in = CatalogueItemPropertyIn(**catalogue_item_property.model_dump())
 
+        # New catalogue category data
+        catalogue_category_in = CatalogueCategoryIn(**stored_catalogue_category.model_dump())
+        catalogue_category_in.catalogue_item_properties.append(catalogue_item_property_in)
+
         # Run all subsequent edits within a transaction to ensure they will all succeed or fail together
         with mongodb_client.start_session() as session:
             with session.start_transaction():
+                # Firstly update the catalogue category
+                # self._catalogue_category_repository.update(
+                #     catalogue_category_id, catalogue_category_in, session=session
+                # )
+
+                # ids = list(
+                #     self._catalogue_item_repository._catalogue_items_collection.find(
+                #         {"catalogue_category_id": ObjectId(catalogue_category_id)}, {"_id": 1}
+                #     )
+                # )
+                # start_time = time.time()
+                # self._catalogue_item_repository._catalogue_items_collection.update_many(
+                #     {"catalogue_category_id": catalogue_category_id}, {"$set": {"catalogue_item_properties": []}}
+                # )
+                # ids = list(
+                #     self._catalogue_item_repository._catalogue_items_collection.find(
+                #         {"catalogue_category_id": ObjectId(catalogue_category_id)}, {"_id": 1}
+                #     )
+                # )
+                # self._catalogue_item_repository._catalogue_items_collection.update_many(
+                #     {"_id": {"$in": ids}}, {"$set": {"catalogue_item_properties": []}}
+                # )
+                # for id in ids:
+                #     self._catalogue_item_repository._catalogue_items_collection.update_many(
+                #         {"_id": id}, {"$set": {"catalogue_item_properties": []}}
+                #     )
+                # updates = []
+                # for id in ids:
+                #     updates.append(UpdateMany({"_id": id}, {"$set": {"catalogue_item_properties": []}}))
+                # self._catalogue_item_repository._catalogue_items_collection.bulk_write(updates, session=session)
+                # logger.debug(ids)
+                # print(len(ids))
+                # logger.debug("Time taken %s", (time.time() - start_time))
+
+                # Now need to update catalogue items and items
+                # raise InvalidActionError("AVOID ACTUAL UPDATE")
                 pass
 
         return CatalogueItemPropertyOut(**catalogue_item_property_in.model_dump())
