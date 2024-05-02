b3cffd3dafc7dbc411ae73fc150c547f945ed271
Joel Davies <joel.davies@stfc.ac.uk>
2024-05-02 11:32:49 +0000

    Minor tweaks #259

--- inventory_management_system_api/services/catalogue_category_property.py
+++ inventory_management_system_api/services/catalogue_category_property.py
@@ -55,7 +55,7 @@ class CatalogueCategoryPropertyService:
         Property will be propagated down through catalogue items and items when there are children.
 
         :param catalogue_category_id: ID of the catalogue category to add the property to
-        :catalogue_item_property: Property to add (with additional info on how to perform the migration if necessary)
+        :param catalogue_item_property: Property to add (with additional info on how to perform the migration if necessary)
         :raises InvalidActionError: If attempting to add a mandatory property without a default_value being specified
                                     or if the catalogue category is not a leaf
         :raises MissingRecordError: If the catalogue category doesn't exist
