da2b5ba34660e5830446f612b9c75876b1cccbce
Joel Davies <joel.davies@stfc.ac.uk>
2024-05-02 09:24:06 +0000

    Use specific function for inserting property into category #259

--- test/unit/repositories/mock_models.py
+++ test/unit/repositories/mock_models.py
@@ -11,6 +11,14 @@ MOCK_CREATED_MODIFIED_TIME = {
     "modified_time": datetime(2024, 2, 16, 14, 1, 13, 0, tzinfo=timezone.utc),
 }
 
+MOCK_CATALOGUE_ITEM_PROPERTY_A_INFO = {
+    "id": str(ObjectId()),
+    "name": "Property A",
+    "type": "number",
+    "unit": "mm",
+    "mandatory": False,
+}
+
 MOCK_PROPERTY_A_INFO = {
     "id": str(ObjectId()),
     "name": "Property A",
