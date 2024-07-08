47b280492733184a4502560cc1f70f6327c072f8
VKTB <45173816+VKTB@users.noreply.github.com>
2024-07-08 13:58:04 +0100

    Clean up manufacturer.py service module #312

--- inventory_management_system_api/services/manufacturer.py
+++ inventory_management_system_api/services/manufacturer.py
@@ -19,23 +19,25 @@ logger = logging.getLogger()
 
 
 class ManufacturerService:
-    """Service for managing manufacturers"""
+    """
+    Service for managing manufacturers.
+    """
 
     def __init__(
         self,
         manufacturer_repository: Annotated[ManufacturerRepo, Depends(ManufacturerRepo)],
     ) -> None:
         """
-        Initialise the manufacturer service with a ManufacturerRepo
+        Initialise the `ManufacturerService` with a `ManufacturerRepo` repository.
 
         :param manufacturer_repository: The `ManufacturerRepo` repository to use.
         """
-
         self._manufacturer_repository = manufacturer_repository
 
     def create(self, manufacturer: ManufacturerPostSchema) -> ManufacturerOut:
         """
         Create a new manufacturer.
+
         :param manufacturer: The manufacturer to be created.
         :return: The created manufacturer.
         """
@@ -52,35 +54,36 @@ class ManufacturerService:
 
     def get(self, manufacturer_id: str) -> Optional[ManufacturerOut]:
         """
-        Get manufacturer by its ID.
+        Retrieve a manufacturer by its ID.
 
-        :param: manufacturer_id: The ID of the requested manufacturer
-        :return: The retrieved manufacturer, or None if not found
+        :param manufacturer_id: The ID of the manufacturer to retrieve.
+        :return: The retrieved manufacturer, or `None` if not found.
         """
         return self._manufacturer_repository.get(manufacturer_id)
 
     def list(self) -> List[ManufacturerOut]:
-        """Get all manufacturers
+        """
+        Retrieve all manufacturers.
 
-        :return: list of all manufacturers
+        :return: List of manufacturers, or empty list if no manufacturers.
         """
         return self._manufacturer_repository.list()
 
     def update(self, manufacturer_id: str, manufacturer: ManufacturerPatchSchema) -> ManufacturerOut:
-        """Update a manufacturer by its ID
-
-
-        :params: manufacturer_id: The ID of the manufacturer to be updated
-        :return: The updated manufacturer
-        :raises MissingRecordError: If manufacturer does not exist in database
         """
-        update_data = manufacturer.model_dump(exclude_unset=True)
+        Update a manufacturer by its ID.
 
+        :params: manufacturer_id: The ID of the manufacturer to be updated.
+        :raises MissingRecordError: If the manufacturer with the given ID does not exist.
+        :return: The updated manufacturer.
+        """
         stored_manufacturer = self.get(manufacturer_id)
         if not stored_manufacturer:
-            raise MissingRecordError(f"No manufacturer found with ID {manufacturer_id}")
+            raise MissingRecordError(f"No manufacturer found with ID: {manufacturer_id}")
 
-        if "name" in update_data and update_data["name"] != stored_manufacturer.name:
+        update_data = manufacturer.model_dump(exclude_unset=True)
+
+        if "name" in update_data and manufacturer.name != stored_manufacturer.name:
             update_data["code"] = utils.generate_code(manufacturer.name, "manufacturer")
 
         stored_manufacturer = stored_manufacturer.model_copy(
@@ -91,9 +94,8 @@ class ManufacturerService:
 
     def delete(self, manufacturer_id: str) -> None:
         """
-        Delete a manufacturer by its ID
+        Delete a manufacturer by its ID.
 
-        :param manufacturer_id: The ID of the manufacturer to delete
-
+        :param manufacturer_id: The ID of the manufacturer to delete.
         """
         return self._manufacturer_repository.delete(manufacturer_id)
