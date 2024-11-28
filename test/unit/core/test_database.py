2541304a59c29e5e8f3a2f5fc30e4bc035b62252
Joel Davies <joel.davies@stfc.ac.uk>
2024-11-28 11:22:30 +0000

    Slightly improve coverage #412

--- test/unit/core/test_database.py
+++ test/unit/core/test_database.py
@@ -25,8 +25,24 @@ def test_start_session_transaction(mock_mongodb_client):
 
 
 @patch("inventory_management_system_api.core.database.mongodb_client")
-def test_start_session_transaction_with_write_conflict(mock_mongodb_client):
-    """Test `start_session_transaction` when there is a write conflict inside the transaction."""
+def test_start_session_transaction_with_operation_failure(mock_mongodb_client):
+    """Test `start_session_transaction` when there is an operation failure inside the transaction."""
+
+    expected_session = mock_mongodb_client.start_session.return_value.__enter__.return_value
+
+    with pytest.raises(OperationFailure) as exc:
+        with start_session_transaction("testing") as session:
+            raise OperationFailure("Some operation error.")
+
+    assert expected_session == session
+    expected_session.start_transaction.assert_called_once()
+    assert str(exc.value) == "Some operation error."
+
+
+@patch("inventory_management_system_api.core.database.mongodb_client")
+def test_start_session_transaction_with_operation_failure_write_conflict(mock_mongodb_client):
+    """Test `start_session_transaction` when there is an operation failure due to a write conflict inside the
+    transaction."""
 
     expected_session = mock_mongodb_client.start_session.return_value.__enter__.return_value
 
