d31cb0cb20bce16fbd1db4f6d3b65f015d033b71
Joel Davies <joel.davies@stfc.ac.uk>
2025-04-15 15:11:17 +0000

    Clean up changes #495

--- scripts/mongodb_entrypoint.sh
+++ scripts/mongodb_entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Reuse the data folder to store the generated keyfile
 KEYFILE_PATH="/data/db/rs_keyfile"
 
 # Generate the replica set keyfile if not already present
@@ -11,20 +12,20 @@ if [ ! -f "$KEYFILE_PATH" ]; then
 fi
 
 # Execute the standard entrypoint to start mongodb, but add the additional parameters to setup the replica set
-# TODO: Put back in docker-compose?
 echo "Starting MongoDB through default entrypoint..."
 /usr/local/bin/docker-entrypoint.sh --replSet "rs0" --keyFile "$KEYFILE_PATH" "$@" &
 
 # Wait for MongoDB to be ready to run commands on
-echo "Waiting for Mongo to be ready..."
-until mongosh --username 'root' --password 'example' --authenticationDatabase=admin --eval "db.adminCommand('ping')" > /dev/null 2>&1; do
+echo "Waiting for MongoDB to be ready..."
+until mongosh --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase=admin --eval "db.adminCommand('ping')" > /dev/null 2>&1; do
   sleep 1
 done
 
 # Initialise replica sets if not already setup
-if ! mongosh --username 'root' --password 'example' --authenticationDatabase=admin --quiet --eval "rs.status().ok" | grep -q '^1$'; then
+if ! mongosh --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase=admin --quiet --eval "rs.status().ok" | grep -q '^1$'; then
   echo "Initializing replica set..."
-  mongosh --username 'root' --password 'example' --authenticationDatabase=admin --eval 'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost:27017" }] })'
+  mongosh --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase=admin \
+    --eval 'rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost:27017" }] })'
 fi
 
 wait
