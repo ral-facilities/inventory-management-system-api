11b1563a0543e011f2b6db03a3b0bab0590128c3
VKTB <45173816+VKTB@users.noreply.github.com>
2025-05-29 11:50:47 +0000

    Move .env.example file to root of project #514

--- inventory_management_system_api/core/config.py
+++ inventory_management_system_api/core/config.py
@@ -106,7 +106,7 @@ class Config(BaseSettings):
     object_storage: ObjectStorageConfig
 
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).parent.parent / ".env",
+        env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter="__",
         hide_input_in_errors=True,
